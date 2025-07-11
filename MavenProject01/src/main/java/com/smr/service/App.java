b9787f58b6c08b1b76663243eff1c4f4fb9712e4
MR.SAMIR <samirkumarpanjha@gmail.com>
2025-07-11 09:07:16 +0530

    division method added by Mr.SAMIR

--- MavenProject01/src/main/java/com/smr/service/App.java
+++ MavenProject01/src/main/java/com/smr/service/App.java
@@ -15,6 +15,10 @@ public class App
 		return x*y;
 	}
 	
+	public int div(int x , int y) {
+		return x/y;
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
