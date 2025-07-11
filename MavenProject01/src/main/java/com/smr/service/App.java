ac02080dfc29a27885551d7952f01c847554c64d
MR.SAMIR <samirkumarpanjha@gmail.com>
2025-07-11 08:10:08 +0530

    TL modified the B.java

--- MavenProject01/src/main/java/com/smr/service/App.java
+++ MavenProject01/src/main/java/com/smr/service/App.java
@@ -6,10 +6,20 @@ public class App
 	public int sum(int x, int y) {
 		return x+y;
 	}
+	
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	
+	public int mul(int x, int y) {
+		return x*y;
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         App app = new App();
         System.out.println("sum is ::" + app.sum(10, 20));
+        System.out.println("sub is ::" + app.sub(10, 20));
     }
 }
