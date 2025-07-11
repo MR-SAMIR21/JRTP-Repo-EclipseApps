dff97ad8612ce412e0f102a4b9ddec113ddd07f8
MR.SAMIR <samirkumarpanjha@gmail.com>
2025-07-11 15:04:41 +0530

    TL - created Payment Branch and added UPI Payment class

---
+++ MavenProject01/src/main/java/com/smr/payment/UPI_Payment.java
@@ -0,0 +1,9 @@
+package com.smr.payment;
+
+public class UPI_Payment {
+
+	public String doPayment(long phoneno, double amount) {
+		System.out.println("Payment is done");
+		return "Payment is done";
+	}
+}
